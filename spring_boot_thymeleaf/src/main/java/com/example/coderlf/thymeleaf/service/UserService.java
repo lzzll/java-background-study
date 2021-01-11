9523d7a276863a903442b735facf2b71949f78b3
lf <736892596>
2021-01-11 18:17:36 +0800

    springBoot中集成thyemleaf

---
+++ spring_boot_thymeleaf/src/main/java/com/example/coderlf/thymeleaf/service/UserService.java
@@ -0,0 +1,49 @@
+package com.example.coderlf.thymeleaf.service;
+
+
+import com.example.coderlf.thymeleaf.model.User;
+
+import java.util.List;
+
+/**
+ * The interface User service.
+ *
+ * @author Liaozihong
+ */
+public interface UserService {
+    /**
+     * Gets user list.
+     *
+     * @return the user list
+     */
+    List<User> getUserList();
+
+    /**
+     * Find user by id user.
+     *
+     * @param id the id
+     * @return the user
+     */
+    User findUserById(long id);
+
+    /**
+     * Save.
+     *
+     * @param user the user
+     */
+    void save(User user);
+
+    /**
+     * Edit.
+     *
+     * @param user the user
+     */
+    void edit(User user);
+
+    /**
+     * Delete.
+     *
+     * @param id the id
+     */
+    void delete(long id);
+}
